44650afbd2dde97ad3bdbf93306aab11885df12c
Louis Hocquet <louis.hocquet@sfr.fr>
2023-09-01 18:25:43 +0200

    Refactored task controller, routes, form partial

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "tasks", to: "tasks#index", as: :tasks
+  # get "tasks", to: "tasks#index", as: :tasks
 
 
-  get "tasks/new", to: "tasks#new", as: :new_task
-  post "tasks", to: "tasks#create"
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # post "tasks", to: "tasks#create"
 
-  get "tasks/:id", to: "tasks#show", as: :task
+  # get "tasks/:id", to: "tasks#show", as: :task
 
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
-  patch "tasks/:id", to: "tasks#update"
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
 
-  delete "tasks/:id", to: "tasks#destroy"
+  # delete "tasks/:id", to: "tasks#destroy"
+
+  # Can be refactored with resources
+  resources :tasks
 end
